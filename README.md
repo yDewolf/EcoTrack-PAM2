00fecc199ce8eaff3596c0bc6cc7f9160bf2e824
yDewolf <andrersng@gmail.com>
2026-02-27 14:07:38 -0300

    Base flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecotrack
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:ecotrack/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LoginScreen());
+}
