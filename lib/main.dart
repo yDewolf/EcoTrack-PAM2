e91a88c8efc7854d431cb03c5b4aef3e2ca4ce70
yDewolf <andrersng@gmail.com>
2026-02-27 14:45:36 -0300

    Tela de login (WIP)

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@ import 'package:ecotrack/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LoginScreen());
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: LoginScreen()
+    );
+  }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController emailTxtController = TextEditingController();
+  final TextEditingController passwordTxtController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('EcoTrack eba!!!'))),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "EcoTrack",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.green,
+              ),
+            ),
+            Column(
+              spacing: 10.0,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: emailTxtController,
+                  decoration: InputDecoration(
+                    label: Text("Email"),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                ),
+                TextField(
+                  controller: passwordTxtController,
+                  decoration: InputDecoration(
+                    label: Text("Senha"),
+                    prefixIcon: Icon(Icons.password),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
