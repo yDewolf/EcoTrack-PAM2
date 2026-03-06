fcbb9712adeac9c1974ef63b1f53dcb071555846
yDewolf <andrersng@gmail.com>
2026-03-06 14:36:16 -0300

    Add logo and buttons

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,40 +15,112 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "EcoTrack",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Colors.green,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Stack(
+            children: [
+              // Container(
+              //   height: 500,
+              //   decoration: BoxDecoration(
+              //     gradient: LinearGradient(
+              //       begin: Alignment.bottomCenter,
+              //       end: Alignment.topCenter,
+              //       colors: [
+              //         const Color.fromARGB(145, 76, 175, 79),
+              //         const Color.fromARGB(0, 76, 223, 106),
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  spacing: 50,
+                  children: [
+                    SizedBox(
+                      height: 200,
+                      width: 400,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            height: double.infinity,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                colors: [
+                                  const Color.fromARGB(145, 76, 175, 79),
+                                  const Color.fromARGB(0, 76, 223, 106),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SvgPicture.asset(
+                            height: double.infinity,
+                            "horizontal_logo.svg",
+                            fit: BoxFit.contain,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 500,
+                      child: Column(
+                        spacing: 10.0,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextField(
+                            controller: emailTxtController,
+                            decoration: InputDecoration(
+                              label: Text("Email"),
+                              prefixIcon: Icon(Icons.email),
+                            ),
+                          ),
+                          TextField(
+                            controller: passwordTxtController,
+                            decoration: InputDecoration(
+                              label: Text("Senha"),
+                              prefixIcon: Icon(Icons.password),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ElevatedButton(
+                                onPressed: () => {print("Login")},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green,
+                                  foregroundColor: Colors.white,
+                                ),
+                                child: Text(
+                                  "Entrar",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () => {print("Esqueci")},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.redAccent,
+                                  foregroundColor: Colors.white,
+                                ),
+                                child: Text(
+                                  "Esqueci minha senha",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Column(
-              spacing: 10.0,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: emailTxtController,
-                  decoration: InputDecoration(
-                    label: Text("Email"),
-                    prefixIcon: Icon(Icons.email),
-                  ),
-                ),
-                TextField(
-                  controller: passwordTxtController,
-                  decoration: InputDecoration(
-                    label: Text("Senha"),
-                    prefixIcon: Icon(Icons.password),
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
